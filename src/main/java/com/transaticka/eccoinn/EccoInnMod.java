e5d4c8b63ed2a801007b222c917c82255eee97ec
Charlie Simonsson <83835492+merberol@users.noreply.github.com>
2022-03-25 20:44:41 +0100

    created dev branch (#2)

--- src/main/java/com/transaticka/eccoinn/EccoInnMod.java
+++ src/main/java/com/transaticka/eccoinn/EccoInnMod.java
@@ -8,8 +8,8 @@ public class EccoInnMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "transtickaexamplemod";
-	public static final Logger LOGGER = LoggerFactory.getLogger("MOD_ID");
+	public static final String MOD_ID = "eccoinnmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -17,6 +17,6 @@ public class EccoInnMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Loading Ecco Inn RP mod");
 	}
 }
